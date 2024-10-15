971d51f8a23c34abd27f3b8918361b36cb178116
Danni Garcia <jodadev.col@gmail.com>
2024-10-15 22:08:50 +1100

    Refactor font imports and update layout styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,107 @@
-import Image from "next/image";
+import { BsCart2 } from 'react-icons/bs'
+import { TiMinus, TiPlus } from 'react-icons/ti'
+
+import { Button } from '@/components/ui/button'
+import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel'
+import Image from 'next/image'
+
+import ProductImage1 from '@/app/images/image-product-1.jpg'
+import ProductImage2 from '@/app/images/image-product-2.jpg'
+import ProductImage3 from '@/app/images/image-product-3.jpg'
+import ProductImage4 from '@/app/images/image-product-4.jpg'
+import { setCurrency } from '@/lib/utils'
+import { Badge } from '@/components/ui/badge'
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className='min-h-screen w-full grid place-content-center'>
+      <section className='max-w-screen-xl px-5 lg:px-10'>
+        <article className='flex items-center justify-center gap-x-10 lg:gap-x-20 xl:gap-x-32 w-full'>
+          <div className='order-2 flex-grow max-w-[445px]'>
+            <header>
+              <div>
+                <h2 className='mb-7'>
+                  <span className='text-xs font-bold text-primary uppercase tracking-widest'>Sneaker Company</span>
+                </h2>
+                <h3 className='mb-8'>
+                  <span className='text-5xl font-bold text-typography-foreground'>Fall Limited Edition Sneakers</span>
+                </h3>
+              </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+              <div className='mb-6'>
+                <p className='text-typography'>
+                  These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.
+                </p>
+              </div>
+            </header>
+
+            <footer className='space-y-8'>
+              <div>
+                <div className='flex items-center justify-start gap-x-4 mb-3'>
+                  <span className='font-bold text-3xl text-typography-foreground'>{setCurrency({ quantity: 125 })}</span>
+                  <span>
+                    <Badge>50%</Badge>
+                  </span>
+                </div>
+
+                <span className='text-muted-foreground text-base font-bold'>{setCurrency({ quantity: 250 })}</span>
+              </div>
+
+              <div className='flex items-center justify-between gap-x-4 w-full'>
+                <div className='bg-secondary inline-flex items-center justify-center w-full max-w-40'>
+                  <Button variant={'secondary'} size={'icon'}>
+                    <TiMinus />
+                  </Button>
+                  <span>0</span>
+
+                  <Button variant={'secondary'} size={'icon'}>
+                    <TiPlus />
+                  </Button>
+                </div>
+
+                <Button className='gap-x-4 w-full'>
+                  <span>
+                    <BsCart2 width={20} height={20} />
+                  </span>
+                  <span>Add to cart</span>
+                </Button>
+              </div>
+            </footer>
+          </div>
+
+          <div className='order-1 flex-grow max-w-[445px]'>
+            <Carousel className='rounded-2xl overflow-hidden'>
+              <CarouselContent>
+                <CarouselItem>
+                  <div className=''>
+                    <Image src={ProductImage1} alt='Image' width={445} height={445} />
+                  </div>
+                </CarouselItem>
+
+                <CarouselItem>
+                  <div className=''>
+                    <Image src={ProductImage2} alt='Image' width={445} height={445} />
+                  </div>
+                </CarouselItem>
+
+                <CarouselItem>
+                  <div className=''>
+                    <Image src={ProductImage3} alt='Image' width={445} height={445} />
+                  </div>
+                </CarouselItem>
+
+                <CarouselItem>
+                  <div className=''>
+                    <Image src={ProductImage4} alt='Image' width={445} height={445} />
+                  </div>
+                </CarouselItem>
+              </CarouselContent>
+            </Carousel>
+
+            <div></div>
+          </div>
+        </article>
+      </section>
     </div>
-  );
+  )
 }
